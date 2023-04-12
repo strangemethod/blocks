4346946e622d5f7648a6c3d3b27a0ab58f6937ac
Andrew Bowles <>
2023-04-12 12:46:10 -0700

    Snap resized column to grid

--- src/js/functions/resize-grid.js
+++ src/js/functions/resize-grid.js
@@ -2,16 +2,20 @@ import {SELECTORS} from "../constants.js"
 
 export default class ResizeGrid {
 	constructor(grid) {
-		this.grid = grid;
-		this.resizeHandles = grid.querySelectorAll(SELECTORS.grid_resize);
-		// todo: recalc on window resize.
-		this.gridWidth = grid.offsetWidth;
-		this.gridCol;
-		this.nextCol;
-		this.gridColWidth;
-		this.nextColWidth;
 		this.dragging = false;
 		this.dragStartPos;
+		this.col;
+		this.colCount = 12;
+		this.colPercent = (1 / this.colCount) * 100;
+		this.colWidth;
+		this.grid = grid;
+		// todo: recalc on window resize.
+		this.gridWidth = grid.offsetWidth;
+		this.newNextCol;
+		this.newNextColWidth;
+		this.nextCol;
+		this.nextColWidth;
+		this.resizeHandles = grid.querySelectorAll(SELECTORS.grid_resize);
 
 		this.bindEventListeners();
 	}
@@ -34,6 +38,13 @@ export default class ResizeGrid {
 		});
 	}
 
+	/* 
+	 * Get Nearest column number to current percentage-based position.
+	 */
+	getNearestCol(width) {
+		return Math.round(width / this.colPercent);
+	}
+
 	/*
 	 * Mouse down handler.
 	 */
@@ -42,11 +53,11 @@ export default class ResizeGrid {
 			this.dragging = true;    
 			this.dragStartPos = e.clientX;
 			
-			this.gridCol = e.target.parentElement;
-			const gridColFlexBasis = window.getComputedStyle(this.gridCol).flexBasis;
-			this.gridColWidth = parseFloat(gridColFlexBasis) / 100.0;
+			this.col = e.target.parentElement;
+			const colFlexBasis = window.getComputedStyle(this.col).flexBasis;
+			this.colWidth = parseFloat(colFlexBasis) / 100.0;
 
-			this.nextCol = this.gridCol.nextElementSibling;
+			this.nextCol = this.col.nextElementSibling;
 			const nextColFlexBasis = window.getComputedStyle(this.nextCol).flexBasis;
 			this.nextColWidth = parseFloat(nextColFlexBasis) / 100.0;
 		}
@@ -57,6 +68,13 @@ export default class ResizeGrid {
 	 */
 	handleMouseUp() {
 		if (this.dragging) this.dragging = false;
+		// Snap to nearest column;
+		this.newColWidth = this.getNearestCol(this.newColWidth) * this.colPercent;
+		this.newNextColWidth = this.getNearestCol(this.newNextColWidth) * this.colPercent;
+		
+		// Update column widths with inline style.
+		this.col.style.flexBasis = `${this.newColWidth}%`;
+		this.nextCol.style.flexBasis = `${this.newNextColWidth}%`;
 	}
 
 	/*
@@ -66,11 +84,12 @@ export default class ResizeGrid {
 		if (this.dragging) {
 			const positionDelta = e.clientX - this.dragStartPos;
 			const percentDelta = (positionDelta / this.gridWidth);
-			const gridColWidthNew = (this.gridColWidth + percentDelta) * 100 + '%';
-			const nextColWidthNew = (this.nextColWidth - percentDelta) * 100 + '%';
+			this.newColWidth = (this.colWidth + percentDelta) * 100;
+			this.newNextColWidth = (this.nextColWidth - percentDelta) * 100;
 
-			this.gridCol.style.flexBasis = gridColWidthNew;
-			this.nextCol.style.flexBasis = nextColWidthNew;
+			// Update column widths with inline style.
+			this.col.style.flexBasis = `${this.newColWidth}%`;
+			this.nextCol.style.flexBasis = `${this.newNextColWidth}%`;
 		}
 	}
 };
